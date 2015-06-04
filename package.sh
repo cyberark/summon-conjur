04a87b03a288b41778367e2f8f3c51ee4a1074cf
Dustin Collins <dustinrcollins@gmail.com>
2015-06-04 13:13:27 -0400

    Add changelog, package script

---
+++ package.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# Get the version from the command line
+if [ -z $VERSION ]; then
+    VERSION=$(git describe --abbrev=0 --tags)
+fi
+
+app="cauldron-conjur"
+
+# Zip and copy to the dist dir
+echo "==> Packaging..."
+rm -rf ./pkg/dist
+mkdir -p ./pkg/dist
+
+for PLATFORM in $(find ./pkg -mindepth 1 -maxdepth 1 -type d); do
+    OSARCH=$(basename ${PLATFORM})
+
+    if [ $OSARCH = "dist" ]; then
+        continue
+    fi
+
+    echo "--> ${OSARCH}"
+    pushd $PLATFORM >/dev/null 2>&1
+    zip ../dist/${app}_${VERSION}_${OSARCH}.zip ./*
+    popd >/dev/null 2>&1
+done
+
+# Make the checksums
+echo "==> Checksumming..."
+pushd ./pkg/dist >/dev/null 2>&1
+shasum -a256 * > ./${app}_${VERSION}_SHA256SUMS
+popd >/dev/null 2>&1
