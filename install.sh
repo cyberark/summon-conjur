60533d1b47376b6e1b7807305f6cc8adc327ba11
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-05-10 10:46:35 -0600

    Adds Check for `sudo` during install (#28)
    * Use `sudo` only if present.  This allows the install script to install into containers
    
    * Move directory creation into sudo check block
    
    * Use same test for sudo as in cyberark/summon installer

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -e
+#!/bin/bash -e
 
 set -e
 
@@ -47,8 +47,13 @@ do_download ${URL} ${ZIP_PATH}
 
 echo "Installing summon-conjur ${LATEST_VERSION} into /usr/local/lib/summon"
 
-sudo mkdir -p /usr/local/lib/summon
-sudo tar -C /usr/local/lib/summon -zxvf ${ZIP_PATH}
+if sudo -h >/dev/null 2>&1; then
+  sudo mkdir -p /usr/local/lib/summon
+  sudo tar -C /usr/local/lib/summon -zxvf ${ZIP_PATH}
+else
+  mkdir -p /usr/local/lib/summon
+  tar -C /usr/local/lib/summon -zxvf ${ZIP_PATH}
+fi
 
 echo "Success!"
 echo "Run /usr/local/lib/summon/summon-conjur for usage"
