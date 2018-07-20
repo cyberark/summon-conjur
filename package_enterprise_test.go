31fd6c994bf9ddf33f9481a3d1d25b3eb0a93aa7
Dustin Collins <dustin.collins@cyberark.com>
2018-07-20 15:21:07 -0500

    remove no-op Given a compiled summon-conjur package context in tests

--- package_enterprise_test.go
+++ package_enterprise_test.go
@@ -19,50 +19,48 @@ func TestPackageEnterprise(t *testing.T) {
 
 	Path := os.Getenv("PATH")
 
-	Convey("Given a compiled summon-conjur package", t, func() {
-		Convey("Given valid V4 appliance configuration", func() {
-			e := ClearEnv()
-			defer e.RestoreEnv()
-			os.Setenv("PATH", Path)
+	Convey("Given valid V4 appliance configuration", func() {
+		e := ClearEnv()
+		defer e.RestoreEnv()
+		os.Setenv("PATH", Path)
 
-			os.Setenv("CONJUR_MAJOR_VERSION", "4")
-			os.Setenv("CONJUR_APPLIANCE_URL", ApplianceURL_V4)
-			os.Setenv("CONJUR_ACCOUNT", Account)
-			os.Setenv("CONJUR_AUTHN_LOGIN", Login)
-			os.Setenv("CONJUR_SSL_CERTIFICATE", SSLCert_V4)
+		os.Setenv("CONJUR_MAJOR_VERSION", "4")
+		os.Setenv("CONJUR_APPLIANCE_URL", ApplianceURL_V4)
+		os.Setenv("CONJUR_ACCOUNT", Account)
+		os.Setenv("CONJUR_AUTHN_LOGIN", Login)
+		os.Setenv("CONJUR_SSL_CERTIFICATE", SSLCert_V4)
 
-			Convey("Given valid APIKey credentials", func() {
-				os.Setenv("CONJUR_AUTHN_API_KEY", APIKey_V4)
+		Convey("Given valid APIKey credentials", func() {
+			os.Setenv("CONJUR_AUTHN_API_KEY", APIKey_V4)
 
-				WithoutArgs()
+			WithoutArgs()
 
-				Convey("Retrieves existing variable's defined value", func() {
-					variableIdentifier := "existent-variable-with-defined-value"
-					secretValue := "existent-variable-defined-value"
+			Convey("Retrieves existing variable's defined value", func() {
+				variableIdentifier := "existent-variable-with-defined-value"
+				secretValue := "existent-variable-defined-value"
 
-					stdout, _, err := RunCommand(PackageName, variableIdentifier)
+				stdout, _, err := RunCommand(PackageName, variableIdentifier)
 
-					So(err, ShouldBeNil)
-					So(stdout.String(), ShouldEqual, secretValue)
-				})
+				So(err, ShouldBeNil)
+				So(stdout.String(), ShouldEqual, secretValue)
+			})
 
-				Convey("Returns error on existent-variable-undefined-value", func() {
-					variableIdentifier := "existent-variable-with-undefined-value"
+			Convey("Returns error on existent-variable-undefined-value", func() {
+				variableIdentifier := "existent-variable-with-undefined-value"
 
-					_, stderr, err := RunCommand(PackageName, variableIdentifier)
+				_, stderr, err := RunCommand(PackageName, variableIdentifier)
 
-					So(err, ShouldNotBeNil)
-					So(stderr.String(), ShouldContainSubstring, "Not Found")
-				})
+				So(err, ShouldNotBeNil)
+				So(stderr.String(), ShouldContainSubstring, "Not Found")
+			})
 
-				Convey("Returns error on non-existent variable", func() {
-					variableIdentifier := "non-existent-variable"
+			Convey("Returns error on non-existent variable", func() {
+				variableIdentifier := "non-existent-variable"
 
-					_, stderr, err := RunCommand(PackageName, variableIdentifier)
+				_, stderr, err := RunCommand(PackageName, variableIdentifier)
 
-					So(err, ShouldNotBeNil)
-					So(stderr.String(), ShouldContainSubstring, "not found")
-				})
+				So(err, ShouldNotBeNil)
+				So(stderr.String(), ShouldContainSubstring, "not found")
 			})
 		})
 	})
--- package_oss_test.go
+++ package_oss_test.go
@@ -24,184 +24,182 @@ func TestPackageOSS(t *testing.T) {
 
 	Path := os.Getenv("PATH")
 
-	Convey("Given a compiled summon-conjur package", t, func() {
-		Convey("Given no configuration and no authentication information", func() {
-			e := ClearEnv()
-			defer e.RestoreEnv()
-			os.Setenv("PATH", Path)
+	Convey("Given no configuration and no authentication information", func() {
+		e := ClearEnv()
+		defer e.RestoreEnv()
+		os.Setenv("PATH", Path)
+
+		WithoutArgs()
+	})
+
+	Convey("Given valid configuration", func() {
+		e := ClearEnv()
+		defer e.RestoreEnv()
+		os.Setenv("PATH", Path)
+
+		os.Setenv("CONJUR_APPLIANCE_URL", ApplianceURL)
+		os.Setenv("CONJUR_ACCOUNT", Account)
+
+		Convey("Given valid APIKey credentials", func() {
+			os.Setenv("CONJUR_AUTHN_LOGIN", Login)
+			os.Setenv("CONJUR_AUTHN_API_KEY", APIKey)
 
 			WithoutArgs()
-		})
 
-		Convey("Given valid configuration", func() {
-			e := ClearEnv()
-			defer e.RestoreEnv()
-			os.Setenv("PATH", Path)
-
-			os.Setenv("CONJUR_APPLIANCE_URL", ApplianceURL)
-			os.Setenv("CONJUR_ACCOUNT", Account)
-
-			Convey("Given valid APIKey credentials", func() {
-				os.Setenv("CONJUR_AUTHN_LOGIN", Login)
-				os.Setenv("CONJUR_AUTHN_API_KEY", APIKey)
-
-				WithoutArgs()
-
-				Convey("Retrieves existing variable's defined value", func() {
-					variableIdentifier := "db/password"
-					secretValue := fmt.Sprintf("secret-value-%v", rand.Intn(123456))
-					policy := fmt.Sprintf(`
+			Convey("Retrieves existing variable's defined value", func() {
+				variableIdentifier := "db/password"
+				secretValue := fmt.Sprintf("secret-value-%v", rand.Intn(123456))
+				policy := fmt.Sprintf(`
 - !variable %s
 `, variableIdentifier)
 
-					config := conjurapi.Config{
-						ApplianceURL: ApplianceURL,
-						Account:      Account,
-					}
-					conjur, _ := conjurapi.NewClientFromKey(config, conjur_authn.LoginPair{Login, APIKey})
+				config := conjurapi.Config{
+					ApplianceURL: ApplianceURL,
+					Account:      Account,
+				}
+				conjur, _ := conjurapi.NewClientFromKey(config, conjur_authn.LoginPair{Login, APIKey})
 
-					conjur.LoadPolicy(
-						conjurapi.PolicyModePost,
-						"root",
-						strings.NewReader(policy),
-					)
-					defer conjur.LoadPolicy(
-						conjurapi.PolicyModePut,
-						"root",
-						strings.NewReader(""),
-					)
+				conjur.LoadPolicy(
+					conjurapi.PolicyModePost,
+					"root",
+					strings.NewReader(policy),
+				)
+				defer conjur.LoadPolicy(
+					conjurapi.PolicyModePut,
+					"root",
+					strings.NewReader(""),
+				)
 
-					conjur.AddSecret(variableIdentifier, secretValue)
+				conjur.AddSecret(variableIdentifier, secretValue)
 
-					stdout, _, err := RunCommand(PackageName, variableIdentifier)
-
-					So(err, ShouldBeNil)
-					So(stdout.String(), ShouldEqual, secretValue)
-				})
-
-				Convey("Returns error on non-existent variable", func() {
-					variableIdentifier := "non-existent-variable"
-
-					_, stderr, err := RunCommand(PackageName, variableIdentifier)
-
-					So(err, ShouldNotBeNil)
-					So(stderr.String(), ShouldContainSubstring, "not found")
-				})
-
-				Convey("Given a non-existent Login is set", func() {
-					os.Setenv("CONJUR_AUTHN_LOGIN", "non-existent-user")
-
-					Convey("Returns 401", func() {
-						variableIdentifier := "existent-or-non-existent-variable"
-
-						_, stderr, err := RunCommand(PackageName, variableIdentifier)
-
-						So(err, ShouldNotBeNil)
-						So(stderr.String(), ShouldContainSubstring, "401")
-					})
-				})
-			})
-
-			Convey("Given valid TokenFile credentials", func() {
-
-				getToken := fmt.Sprintf(`
-token=$(curl --data "%s" "$CONJUR_APPLIANCE_URL/authn/$CONJUR_ACCOUNT/%s/authenticate")
-echo $token
-`, APIKey, Login)
-				stdout, _, err := RunCommand("bash", "-c", getToken)
+				stdout, _, err := RunCommand(PackageName, variableIdentifier)
 
 				So(err, ShouldBeNil)
-				So(stdout.String(), ShouldContainSubstring, "signature")
-
-				tokenFile, _ := ioutil.TempFile("", "existent-token-file")
-				tokenFileName := tokenFile.Name()
-				tokenFileContents := stdout.String()
-				os.Remove(tokenFileName)
-				go func() {
-					ioutil.WriteFile(tokenFileName, []byte(tokenFileContents), 0600)
-				}()
-				defer os.Remove(tokenFileName)
-
-				os.Setenv("CONJUR_AUTHN_TOKEN_FILE", tokenFileName)
-
-				WithoutArgs()
-
-				Convey("Retrieves existent variable's defined value", func() {
-					variableIdentifier := "db/password"
-					secretValue := fmt.Sprintf("secret-value-%v", rand.Intn(123456))
-					policy := fmt.Sprintf(`
-- !variable %s
-`, variableIdentifier)
-
-					config := conjurapi.Config{
-						ApplianceURL: ApplianceURL,
-						Account:      Account,
-					}
-					conjur, _ := conjurapi.NewClientFromKey(config, conjur_authn.LoginPair{Login, APIKey})
-
-					conjur.LoadPolicy(
-						conjurapi.PolicyModePost,
-						"root",
-						strings.NewReader(policy),
-					)
-					defer conjur.LoadPolicy(
-						conjurapi.PolicyModePut,
-						"root",
-						strings.NewReader(""),
-					)
-
-					conjur.AddSecret(variableIdentifier, secretValue)
-
-					stdout, _, err := RunCommand(PackageName, variableIdentifier)
-
-					So(err, ShouldBeNil)
-					So(stdout.String(), ShouldEqual, secretValue)
-				})
-
-				Convey("Returns error on non-existent variable", func() {
-					variableIdentifier := "non-existent-variable"
-
-					_, stderr, err := RunCommand(PackageName, variableIdentifier)
-
-					So(err, ShouldNotBeNil)
-					So(stderr.String(), ShouldContainSubstring, "not found in account")
-				})
-
-				Convey("Given a non-existent TokenFile is set", func() {
-					os.Setenv("CONJUR_AUTHN_TOKEN_FILE", "non-existent-token-file")
-
-					Convey("Waits for longer than a second", func() {
-						timeout := time.After(1 * time.Second)
-						unexpected_response := make(chan int)
-
-						go func() {
-							variableIdentifier := "existent-or-non-existent-variable"
-							RunCommand(PackageName, variableIdentifier)
-							unexpected_response <- 1
-						}()
-
-						select {
-						case <-unexpected_response:
-							So("receive unexpected response", ShouldEqual, "not receive unexpected response")
-						case <-timeout:
-							So(true, ShouldEqual, true)
-						}
-					})
-				})
+				So(stdout.String(), ShouldEqual, secretValue)
 			})
 
-			Convey("Given no authentication credentials", func() {
+			Convey("Returns error on non-existent variable", func() {
+				variableIdentifier := "non-existent-variable"
 
-				WithoutArgs()
+				_, stderr, err := RunCommand(PackageName, variableIdentifier)
 
-				Convey("Returns with error on non-existent variable", func() {
+				So(err, ShouldNotBeNil)
+				So(stderr.String(), ShouldContainSubstring, "not found")
+			})
+
+			Convey("Given a non-existent Login is set", func() {
+				os.Setenv("CONJUR_AUTHN_LOGIN", "non-existent-user")
+
+				Convey("Returns 401", func() {
 					variableIdentifier := "existent-or-non-existent-variable"
 
 					_, stderr, err := RunCommand(PackageName, variableIdentifier)
 
 					So(err, ShouldNotBeNil)
-					So(stderr.String(), ShouldContainSubstring, "at least one authentication strategy")
+					So(stderr.String(), ShouldContainSubstring, "401")
 				})
+			})
+		})
+
+		Convey("Given valid TokenFile credentials", func() {
+
+			getToken := fmt.Sprintf(`
+token=$(curl --data "%s" "$CONJUR_APPLIANCE_URL/authn/$CONJUR_ACCOUNT/%s/authenticate")
+echo $token
+`, APIKey, Login)
+			stdout, _, err := RunCommand("bash", "-c", getToken)
+
+			So(err, ShouldBeNil)
+			So(stdout.String(), ShouldContainSubstring, "signature")
+
+			tokenFile, _ := ioutil.TempFile("", "existent-token-file")
+			tokenFileName := tokenFile.Name()
+			tokenFileContents := stdout.String()
+			os.Remove(tokenFileName)
+			go func() {
+				ioutil.WriteFile(tokenFileName, []byte(tokenFileContents), 0600)
+			}()
+			defer os.Remove(tokenFileName)
+
+			os.Setenv("CONJUR_AUTHN_TOKEN_FILE", tokenFileName)
+
+			WithoutArgs()
+
+			Convey("Retrieves existent variable's defined value", func() {
+				variableIdentifier := "db/password"
+				secretValue := fmt.Sprintf("secret-value-%v", rand.Intn(123456))
+				policy := fmt.Sprintf(`
+- !variable %s
+`, variableIdentifier)
+
+				config := conjurapi.Config{
+					ApplianceURL: ApplianceURL,
+					Account:      Account,
+				}
+				conjur, _ := conjurapi.NewClientFromKey(config, conjur_authn.LoginPair{Login, APIKey})
+
+				conjur.LoadPolicy(
+					conjurapi.PolicyModePost,
+					"root",
+					strings.NewReader(policy),
+				)
+				defer conjur.LoadPolicy(
+					conjurapi.PolicyModePut,
+					"root",
+					strings.NewReader(""),
+				)
+
+				conjur.AddSecret(variableIdentifier, secretValue)
+
+				stdout, _, err := RunCommand(PackageName, variableIdentifier)
+
+				So(err, ShouldBeNil)
+				So(stdout.String(), ShouldEqual, secretValue)
+			})
+
+			Convey("Returns error on non-existent variable", func() {
+				variableIdentifier := "non-existent-variable"
+
+				_, stderr, err := RunCommand(PackageName, variableIdentifier)
+
+				So(err, ShouldNotBeNil)
+				So(stderr.String(), ShouldContainSubstring, "not found in account")
+			})
+
+			Convey("Given a non-existent TokenFile is set", func() {
+				os.Setenv("CONJUR_AUTHN_TOKEN_FILE", "non-existent-token-file")
+
+				Convey("Waits for longer than a second", func() {
+					timeout := time.After(1 * time.Second)
+					unexpected_response := make(chan int)
+
+					go func() {
+						variableIdentifier := "existent-or-non-existent-variable"
+						RunCommand(PackageName, variableIdentifier)
+						unexpected_response <- 1
+					}()
+
+					select {
+					case <-unexpected_response:
+						So("receive unexpected response", ShouldEqual, "not receive unexpected response")
+					case <-timeout:
+						So(true, ShouldEqual, true)
+					}
+				})
+			})
+		})
+
+		Convey("Given no authentication credentials", func() {
+
+			WithoutArgs()
+
+			Convey("Returns with error on non-existent variable", func() {
+				variableIdentifier := "existent-or-non-existent-variable"
+
+				_, stderr, err := RunCommand(PackageName, variableIdentifier)
+
+				So(err, ShouldNotBeNil)
+				So(stderr.String(), ShouldContainSubstring, "at least one authentication strategy")
 			})
 		})
 	})
