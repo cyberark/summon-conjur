c400ffaced9476356ba298444029d21b577f1959
Dustin Collins <dustin.collins@cyberark.com>
2018-08-06 14:47:09 -0500

    Use upstream goreleaser image

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 git fetch --tags  # jenkins does not do this automatically yet
 
-docker-compose build --pull goreleaser
+docker-compose pull goreleaser
+
 docker-compose run --rm -T --entrypoint sh goreleaser -es <<EOF
 dep ensure --vendor-only
 goreleaser release --rm-dist --skip-validate
