fb5b4f2a93933b650ef86e288509c708fb0c7c00
Andy Tinkham <andy.tinkham@cyberark.com>
2020-07-31 14:35:39 -0500

    Add test coverage
    Signed-off-by: Andy Tinkham <andy.tinkham@cyberark.com>

--- bin/test-entrypoint.sh
+++ bin/test-entrypoint.sh
@@ -23,8 +23,10 @@ echo "Running go tests: $TEST_PARAMS"
 echo "Current dir: $(pwd)"
 
 set -x
-go test -v ./test/... $TEST_PARAMS | tee output/junit.output
+go test --coverprofile=output/c.out -v ./test/... $TEST_PARAMS | tee output/junit.output
 
 go-junit-report < output/junit.output > output/junit.xml
+
+gocov convert output/c.out | gocov-xml > output/coverage.xml
 
 rm output/junit.output
